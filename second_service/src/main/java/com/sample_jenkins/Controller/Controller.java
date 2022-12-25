dc38151bf53da09b2d3575d98c86e89dc1b6899d
GeorgeKariukiNgugi <ngugigeorge697@gmail.com>
2022-12-25 14:06:17 +0300

    Removed initail files

---
+++ second_service/src/main/java/com/sample_jenkins/Controller/Controller.java
@@ -0,0 +1,27 @@
+package com.sample_jenkins.Controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+
+@RestController
+@RequestMapping(path = "/second_service")
+public class Controller {
+
+    @GetMapping
+    public ResponseEntity<Object> addRule(){
+
+        HashMap data = new HashMap();
+
+        data.put("fist","first");
+        data.put("second","second");
+        data.put("third","third");
+
+        return new ResponseEntity<Object>(data,  HttpStatus.OK);
+    }
+
+}
---
+++ third_service/src/main/java/com/sample_jenkins/Main.java
@@ -0,0 +1,7 @@
+package com.sample_jenkins;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
